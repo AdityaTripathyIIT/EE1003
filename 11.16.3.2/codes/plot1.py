4bab68d898652079758dac5765b73b107c3234b9
AdityaTripathyIIT <adityatripathystudies@gmail.com>
2025-01-26 13:12:10 +0530

    sims

--- 11.16.3.2/codes/plot1.py
+++ 11.16.3.2/codes/plot1.py
@@ -1,27 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import ctypes
 
-def pmf(x):
-    if x == 0:
-        return 0.25
-    elif x == 1:
-        return 0.5
-    elif x == 2:
-        return 0.25
-    else:
-        return 0
-
-x_values = np.array([-0.5, 0, 0.5, 1, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0])
-y_values = np.array([pmf(x) for x in x_values])
-
+gen = ctypes.CDLL('./sim.so')
+gen.theoretical_binomial.argtypes = [ctypes.c_int, ctypes.c_double, ctypes.POINTER(ctypes.c_double)]
+gen.theoretical_binomial.restype = None
+gen.sim_binomial.argtypes = [ctypes.c_int, ctypes.c_double, ctypes.POINTER(ctypes.c_double)]
+gen.sim_binomial.restype = None
+n = 2
+p = 0.5
+x_values = np.array([x for x in range(n + 1)])
+y_values = np.zeros((1, n+1), dtype = np.double)
+gen.theoretical_binomial(
+        n, p, y_values.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        )
+print(x_values)
+print(y_values)
 # Create the stem plot
 plt.figure(figsize=(8, 6))
-markerline, stemlines, baseline = plt.stem(x_values, y_values)
+markerline, stemlines, baseline = plt.stem(x_values, y_values[0], label = "theory")
 plt.setp(markerline, 'markerfacecolor', 'red')
 plt.setp(stemlines, 'color', 'red')
 plt.setp(baseline, 'color', 'gray', 'linewidth', 1)
 
+gen.sim_binomial(
+        n, p, y_values.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        )
+markerline, stemlines, baseline = plt.stem(x_values, y_values[0], label = "sim")
+plt.setp(markerline, 'markerfacecolor', 'green')
+plt.setp(stemlines, 'color', 'blue')
+plt.setp(baseline, 'color', 'gray', 'linewidth', 1)
+plt.legend(loc = "best")
 plt.xlabel('x')
+plt.xlim(-0.5, 2.5)
 plt.ylabel('f(x)')
 plt.grid(True)
 plt.savefig("../figs/fig1.png")
--- 11.16.3.2/codes/plot2.py
+++ 11.16.3.2/codes/plot2.py
@@ -1,21 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import ctypes
 
-def cdf(x):
-    if x < 0:
-        return 0
-    elif 0 <= x < 1:
-        return 0.25
-    elif 1 <= x < 2:
-        return 0.75
-    else:
-        return 1
-x_values = np.linspace(-1, 3, 400)
-y_values = np.array([piecewise_function(x) for x in x_values])
+gen = ctypes.CDLL('./sim.so')
+gen.sim_cdf.argtpes = [ctypes.c_int, ctypes.c_double, ctypes.POINTER(ctypes.c_double)]
+gen.sim_cdf.restype = None
+gen.theoretical_cdf.argtpes = [ctypes.c_int, ctypes.c_double, ctypes.POINTER(ctypes.c_double)]
+gen.theoretical_cdf.restype = None
+
+n = 2 
+p = 0.5
+
+x_values = np.array([x for x in range(n+ 1)])
+y_values = np.zeros((1, n+1), dtype=np.double)
+x_values = np.append(x_values, -1)
+gen.theoretical_cdf(
+        n, ctypes.c_double(p), y_values.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        )
+y_values = np.append(y_values, 0)
+print("Theoretical values of probability: ", y_values)
 plt.figure(figsize=(8, 6))
-plt.plot(x_values, y_values, label='Cumulative Distribution Function', color='blue')
+markerline, stemlines, baseline = plt.stem(x_values, y_values, label = "theory")
+plt.setp(markerline, 'markerfacecolor', 'red')
+plt.setp(stemlines, 'color', 'red')
+plt.setp(baseline, 'color', 'gray', 'linewidth', 1)
+
+y_values = np.zeros((1, n+1), dtype=np.double)
+gen.sim_cdf(
+        n, ctypes.c_double(p), y_values.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        )
+y_values = np.append(y_values, 0)
+print("Simulated values of probability: ", y_values)
+markerline, stemlines, baseline = plt.stem(x_values, y_values, label = "sim")
+plt.setp(markerline, 'markerfacecolor', 'red')
+plt.setp(stemlines, 'color', 'red')
+plt.setp(baseline, 'color', 'gray', 'linewidth', 1)
+
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
+plt.legend(loc='best')
 plt.legend()
 plt.savefig("../figs/fig2.png")
